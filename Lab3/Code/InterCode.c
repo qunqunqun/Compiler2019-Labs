cf951bbb91198dcd5cee801e5cb208f76f139140
Young <895254752@qq.com>
2019-11-27 01:50:35 +0800

    周二晚上周三凌晨

--- Lab3/Code/InterCode.c
+++ Lab3/Code/InterCode.c
@@ -1,2 +1,186 @@
 #include "InterCode.h"
 
+
+// translate函数
+void translateTree(GramTree * root){
+    InterCodes codes = translate_Program(root);
+    printInterCodes(codes);
+}
+
+InterCodes translate_Program(GramTree* root){
+    InterCodes codes = translate_ExtDefList(root->child[0]);
+    return codes;
+}
+
+InterCodes translate_ExtDefList(GramTree* root){
+    // TODO:empty的话会有几个子节点？
+    if(root->nChild == 0 || root->nChild == 1){
+        printf("translate_ExtDefList nChild= %d\n",root->nChild);
+        return NULL;
+    }else{
+        return link2Codes(
+            translate_ExtDef(root->child[0]),
+            translate_ExtDefList(root->child[1]));
+    }
+}
+
+InterCodes translate_ExtDef(GramTree* root){
+    if(root->nChild == 3){
+        if(isEqual(root->child[1]->tag, "FunDec") == 1 && isEqual(root->child[2]->tag, "CompSt") == 1){
+            return link2Codes(
+                translate_FunDec(root->child[1]),
+                translate_CompSt(root->child[2]));
+        }
+    }else{
+        return NULL;
+    }
+}
+
+InterCodes translate_FunDec(GramTree* root){
+    InterCodes funcCodes = getFuncCodes(root->child[0]);
+    if(root->nChild == 3){
+        return funcCodes;
+    }else if(root->nChild == 4 && isEqual(root->child[2]->tag, "VarList") == 1){
+        return link2Codes(funcCodes, translate_VarList(root->child[2]));
+    }else{
+        assert(0);
+    }
+
+}
+
+InterCodes translate_CompSt(GramTree* root){
+
+}
+
+InterCodes translate_VarList(GramTree* root){
+    InterCodes paramDecCodes = translate_ParamDec(root->child[0]);
+    if(root->nChild == 1){
+        return paramDecCodes;
+    }else if(root->nChild == 3 && isEqual(root->child[2]->tag, "VarList") == 1){
+        return link2Codes(paramDecCodes, translate_VarList(root->child[2]));
+    }else{
+        assert(0);
+    }
+}
+
+InterCodes translate_ParamDec(GramTree* root){
+    return translate_VarDec(root->child[0]);
+}
+
+InterCodes translate_VarDec(GramTree* root){
+    while(root->nChild == 4){
+        root = root->child[0];
+    }
+    // 此时Root指向var了
+    Operand op = getVar(root->child[0]->val.a); //FIXME:写到这里就知道有问题了
+}
+
+
+
+
+
+// get函数
+InterCodes getFuncCodes(GramTree* root){
+    // TODO:
+    return NULL;
+}
+
+// TODO:还不知道怎么写才好，先随便写一个
+Operand getVar(int value){
+    Operand op = malloc(sizeof(OperandBody));
+    op->kind = VARIABLE;
+    op->u.value = value;
+    return op;
+} 
+
+
+// 中间代码的连接
+InterCodes link2Codes(InterCodes c1, InterCodes c2){
+    if(c1 == NULL) return c2;
+    if(c2 == NULL) return c1;
+
+    // e.g. ca1<-c1->cb1 , c1->cb1->..->ca1
+    // e.g. ca2<-c2->cb2 , c2->cb2->..->ca2
+    InterCodes t1 = c1->prev;
+    InterCodes t2 = c2->prev;
+    c1->prev = t2;
+    t2->next = c1;
+    c2->prev = t1;
+    t1->next = c2;
+
+    return c1;
+}
+
+InterCodes link3Codes(InterCodes c1, InterCodes c2, InterCodes c3){
+    return link2Codes(c1, link2Codes(c2, c3));
+}
+
+InterCodes link4Codes(InterCodes c1, InterCodes c2, InterCodes c3, InterCodes c4){
+    return link2Codes(c1, link3Codes(c2, c3, c4));
+}
+
+// TODO: 双向循环链表头尾实现的时候接起来吗,如果首尾相接，则需要防止死循环——回答，是的。
+void printInterCodes(InterCodes codes){
+    InterCodes p = codes;
+    if(p != NULL){
+        printInterCode(p->code);
+        p = p->next;
+        while(p != NULL){
+            printInterCode(p->code);
+            p = p->next;
+        }
+    }
+
+}
+
+// TODO:函数需要完善
+char* getOperand(Operand op, int opKind){
+    char* opName = "opName"; // = =随便写个虚假的
+    return opName;
+}
+
+// TODO: 后期可以定向到文件流中输出文件，目前先打印调试
+void printInterCode(InterCode code){
+    switch (code->kind) {
+        case LABLE_IR:
+            printf("LABEL label%d :\n", code->labelNo);
+            break;
+        case FUNC_IR:
+            printf("FUNCTION %s :\n", code->funcName);
+            break;
+        case ASSIGN_IR:
+            printf("%s := %s\n", getOp(code->u.assign.left, code->opKind), getOp(code->u.assign.right, code->opKind));
+            break;
+        case ARITH_IR:
+            printf("%s := %s %c %s\n", getOp(code->u.binop.result, code->opKind), getOp(code->u.binop.op1, code->opKind), code->u.binop.arithType, getOp(code->u.binop.op2, VAL_OP));
+            break;
+        case RELOP_IR:
+            printf("IF %s %s %s ", getOp(code->u.relop.x, code->opKind), code->u.relop.relopType, getOp(code->u.relop.y, code->opKind));
+        case GOTO_IR:
+            printf("GOTO label%d\n", code->labelNo);
+            break;
+        case RETURN_IR:
+            printf("RETURN %s\n", getOp(code->u.single.op, code->opKind));
+            break;
+        case DEC_IR:
+            printf("DEC %s %d\n", getOp(code->u.dec.op, code->opKind), code->u.dec.decSize);
+            break;
+        case ARG_IR:
+            printf("ARG %s\n", getOp(code->u.single.op, code->opKind));
+            break;
+        case CALL_IR:
+            printf("%s := CALL %s\n", getOp(code->u.single.op, code->opKind), code->u.single.funcName);
+            break;
+        case PARAM_IR:
+            printf("PARAM %s\n", getOpName(code->u.single.op));
+            break;
+        case READ_IR:
+            printf("READ %s\n", getOp(code->u.single.op, code->opKind));
+            break;
+        case WRITE_IR:
+            printf("WRITE %s\n", getOp(code->u.single.op, code->opKind));
+            break;
+        default:
+            break;
+    }
+}
--- Lab3/Code/InterCode.h
+++ Lab3/Code/InterCode.h
@@ -1,20 +1,95 @@
 #include "semantic.h"
 
-typedef struct Operand_* Operand;
-struct Operand_ {
+// 址操作与值操作
+enum { ADDR_OP, VAL_OP } ;
+
+// 操作数
+typedef struct Operand_* Operand; 
+typedef struct Operand_ {
     enum { VARIABLE, CONSTANT, ADDRESS } kind;
     union {
         int var_no;
         int value;
     } u;
-};
+}OperandBody;
 
 
-struct InterCode
-{
-    enum { ASSIGN, ADD, SUB, MUL } kind;
+// 中间代码
+typedef struct InterCode_* InterCode;
+typedef struct InterCode_{
+    enum { 
+        LABLE_IR,   //  LABEL x :定义标号x。
+        FUNC_IR,    //  FUNCTION f :定义函数f。
+        ASSIGN_IR,  //  x := y x := &y x := *y *x := y 赋值操作
+        ARITH_IR,   //  x := y + z 加减乘除操作。
+        GOTO_IR,    //  GOTO x 无条件跳转至标号x
+        RELOP_IR,   //  IF x [relop] y GOTO z 如果x与y满足[relop]关系则跳转至标号z。
+        RETURN_IR,  //  RETURN x 退出当前函数并返回x值。
+        DEC_IR,     //  DEC x [size] 内存空间申请，大小为4的倍数。
+        ARG_IR,     //  ARG x 传实参x。
+        CALL_IR,    //  x := CALL f 调用函数，并将其返回值赋给x。
+        PARAM_IR,   //  PARAM x 函数参数声明
+        READ_IR,    //  READ x 从控制台读取x的值
+        WRITE_IR    //  WRITE x 向控制台打印x的值。
+    } kind; 
     union {
+        //  赋值操作：x := y,x := &y,x := *y,*x := y
         struct { Operand right, left; } assign;
-        struct { Operand result, op1, op2; } binop;
+        //  算术操作 x := y + z，x := y - z，x := y * z，x := y / z
+        struct { Operand result, op1, op2; char arithType; } binop;
+        //  LABEL x :，FUNCTION f :，GOTO x，RETURN x，ARG x， x := CALL f，PARAM x，READ x，WRITE x
+        struct { 
+            Operand op;
+            union {
+                char *funcName;
+            };
+        } single; 
+        //  DEC x [size]
+        struct { Operand op; int decSize;} dec;  
+        //  好像只有这个：IF x [relop] y GOTO z
+        struct { Operand x,y; char* relopType;} relop;
     } u;
-};
+    int labelNo;    //有label，才有这个    
+    int opKind;
+}InterCodeBody;
+
+
+// 线性IR的Linked list形式
+typedef struct InterCodes_* InterCodes;
+typedef struct InterCodes_ { 
+    InterCode code; 
+    InterCodes prev, next; 
+}InterCodesBody;
+
+
+// 参数列表
+typedef struct ArgList_* ArgList;
+typedef struct ArgList_ {
+    Operand op;
+    Type opType;
+    ArgList next;
+}ArgListBody;
+
+// translate 函数
+void translateTree(GramTree * root);
+InterCodes translate_Program(GramTree* root);
+InterCodes translate_ExtDefList(GramTree* root);
+InterCodes translate_ExtDef(GramTree* root);
+InterCodes translate_FunDec(GramTree* root);
+InterCodes translate_CompSt(GramTree* root);
+InterCodes translate_VarList(GramTree* root);
+InterCodes translate_ParamDec(GramTree* root);
+InterCodes translate_VarDec(GramTree* root);
+
+// get函数
+InterCodes getFuncCodes(GramTree* root);
+Operand getVar(int value); 
+// 工具人函数
+void printInterCodes(InterCodes codes);
+void printInterCode(InterCode code);
+char* getOperand(Operand op, int opKind);
+
+// 中间代码的连接
+InterCodes link2Codes(InterCodes c1, InterCodes c2);
+InterCodes link3Codes(InterCodes c1, InterCodes c2, InterCodes c3);
+InterCodes link4Codes(InterCodes c1, InterCodes c2, InterCodes c3, InterCodes c4);
