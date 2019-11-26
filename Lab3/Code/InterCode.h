01080c4aa402fe7b306e16d709dad57d948659f7
young <895254752@qq.com>
2019-11-26 19:58:00 +0800

    new data structure

---
+++ Lab3/Code/InterCode.h
@@ -0,0 +1,20 @@
+#include "semantic.h"
+
+typedef struct Operand_* Operand;
+struct Operand_ {
+    enum { VARIABLE, CONSTANT, ADDRESS } kind;
+    union {
+        int var_no;
+        int value;
+    } u;
+};
+
+
+struct InterCode
+{
+    enum { ASSIGN, ADD, SUB, MUL } kind;
+    union {
+        struct { Operand right, left; } assign;
+        struct { Operand result, op1, op2; } binop;
+    } u;
+};
